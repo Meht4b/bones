bed6b6bed88bcb70f125e8fd913ccc702712e786
Mehthab <114091862+Meht4b@users.noreply.github.com>
2024-02-07 22:17:19 +0530

    first commit

---
+++ classes.py
@@ -0,0 +1,57 @@
+import pygame
+from vector import vector
+import math
+
+
+
+class bone:
+
+    bones = []
+    
+    def __init__(self,length,rotation,head=vector(0,0),parent=None):
+    
+        rotation = math.radians(rotation)
+
+        self.head = head
+        self.parent = parent
+        self.rotation = rotation
+        self.angle = rotation
+
+        if parent!=None:
+            
+            print(bone.bones[parent].angle)
+            self.angle = rotation+bone.bones[parent].angle
+            self.head = bone.bones[parent].tail
+
+        self.length = length
+        self.rotation = rotation
+
+        self.tail = self.head + vector(length*math.cos(self.angle),length*math.sin(self.angle))
+
+        bone.bones.append(self)
+
+    def rotate(self,angle):
+        angle = self.rotation+math.radians(angle)
+        self.tail = self.head + vector(self.length*math.cos(angle),self.length*math.sin(angle))
+        self.rotation = angle
+        self.angle = angle
+
+    def update(self,win):
+        
+        if self.parent!=None:
+            self.head = bone.bones[self.parent].tail
+            self.angle = bone.bones[self.parent].angle + self.rotation
+            self.tail = self.head + vector(self.length*math.cos(self.angle),self.length*math.sin(self.angle))
+            
+
+        self.display(win)
+
+    @classmethod
+    def classupdate(cls,win):
+        for i in cls.bones:
+            i.update(win)
+
+    def display(self,win):
+        pygame.draw.line(win,(255,255,255),self.head.tupAdj(),self.tail.tupAdj())
+    
+    
---
+++ sim.py
@@ -0,0 +1,40 @@
+from classes import *
+
+pygame.init()
+
+window = pygame.display.set_mode((1000,1000))
+
+a = bone(100,60,head=vector(100,100))
+b = bone(100,0,parent=0)
+c = bone(100,0,parent=1)
+
+ch = 0
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            break
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+
+                ch +=1
+                if ch == len(bone.bones):
+                    ch =0
+            if event.key == pygame.K_UP:
+
+                ch -=1
+                if ch == 0:
+                    ch = len(bone.bones)
+
+        if event.type == pygame.MOUSEWHEEL:
+            bone.bones[ch].rotate(10*event.y)
+
+            
+    window.fill((0,0,0))
+    #print(bone.bones[0].angle,bone.bones[1].rotation,bone.bones[1].rotation)
+    bone.classupdate(window)
+    
+
+    pygame.display.update()
